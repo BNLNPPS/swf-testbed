b783e01573ccb939a93a4e63af7fc9a1c84734f5
Maxim Potekhin <potekhin@bnl.gov>
2025-08-22 17:54:19 -0400

    Short stub for the simulator added

--- agents/data_agent.py
+++ agents/data_agent.py
@@ -1,23 +1,39 @@
-# This is the initial stub - wrap the Rucio imports for better
-# readbility elsewhere.
 
-
-# Work in progress, this is a stub for the Rucio imports
-# to be used in the data agent.
+# Rucio imports for demonstration purposes only.
 # The actual implementation will be in the rucio_comms package.
 
-from rucio.client import Client as RucioClient
+# from rucio.client import Client as RucioClient
 
-from rucio.client.uploadclient import UploadClient
-from rucio.common.exception import RucioException
-from rucio.common.utils import generate_uuid
-from rucio.common.types import InternalAccount
-from rucio.common.logging import setup_logging
-import logging
-import os
-from typing import Optional, List, Dict, Any
-from rucio.common.utils import parse_replicas
-
+# from rucio.client.uploadclient import UploadClient
+# from rucio.common.exception import RucioException
+# from rucio.common.utils import generate_uuid
+# from rucio.common.types import InternalAccount
+# from rucio.common.logging import setup_logging
+# import logging
+# import os
+# from typing import Optional, List, Dict, Any
+# from rucio.common.utils import parse_replicas
 
 # ---
 
+###################################################################################
+class DATA:
+    ''' The DATA class is the main data management class.
+        It receives messages from the DAQ simulator and handles them.
+        Main functionality is to create Rucio datasets and register files to
+        these datasets. Then, to notify the processing agent that the data is ready.
+    '''
+    def __init__(self,
+                 verbose: bool  = False,
+                 sender         = None,
+                 receiver       = None):
+        self.verbose = verbose
+        self.sender  = sender
+        self.receiver = receiver
+        if self.verbose: print(f'''*** DATA class initialized ***''')
+
+        # Initialize Rucio client, upload client, etc.
+        # self.rucio_client = RucioClient()
+        # self.upload_client = UploadClient()
+        # self.rucio_client = RucioClient(account=InternalAccount('root'))
+        # self.upload_client = UploadClient(account=InternalAccount('root'))
